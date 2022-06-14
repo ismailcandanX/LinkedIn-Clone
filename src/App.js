f2982b0e9192a7591ea2cc2fa354f75594355a81
ismailcandanX <ismailcandan44@gmail.com>
2022-06-14 15:50:07 +0300

    Header is completed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Login from './components/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<><Header /><Home /></>} />
+    </Routes>
   );
 }
 
