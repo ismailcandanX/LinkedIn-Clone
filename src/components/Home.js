41edb205e474d90d23bec835cbb86aefa556e0a3
ismailcandanX <ismailcandan44@gmail.com>
2022-06-17 19:28:22 +0300

    Left side completed.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,19 @@
 import styled from "styled-components";
+import LeftSide from "./LeftSide";
+import Main from "./Main";
+import RightSide from "./RightSide";
 
 const Home = (props) => {
     return <Container>
         <Section>
             <h5><a>Hiring in hurry? - </a></h5>
             <p>Find talented pros in record time with Upwork and keep business moving.</p>
-
         </Section>
+        <Layout>
+            <LeftSide />
+            <Main />
+            <RightSide />
+        </Layout>
     </Container>
 }
 
@@ -46,6 +53,19 @@ const Section = styled.div`
         padding: 0 5px;
     }
 `
-
+const Layout = styled.div`
+    display: grid;
+    grid-template-areas: "leftside main rightside";
+    grid-template-columns: minmax(0,5fr) minmax(0,12fr) minmax(300px, 7fr);
+    column-gap: 25px;
+    row-gap: 25px;
+    /* grid-template-rows: auto; */
+    margin: 25px 0;
+    @media ( max-width: 768px) {
+        display: flex;
+        flex-direction: column;
+        padding: 0 5px;
+    }
+`
 
 export default Home;
