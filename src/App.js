3811945f7e5c74a3815c34f0bd22cae54259db5f
ismailcandanX <ismailcandan44@gmail.com>
2022-06-18 22:26:47 +0300

    Saved active user and details to redux.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import Login from './components/Login';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import Home from './components/Home';
+import { useEffect } from 'react';
+import { getUserAuth } from './actions';
+import { connect } from 'react-redux';
 
-function App() {
+function App(props) {
+  useEffect(() => {
+    props.getUserAuth();
+  }, [])
+
   return (
     <Routes>
       <Route path="/" element={<Login />} />
@@ -13,4 +20,10 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {}
+}
+const mapDispatchToProps = (dispatch) => ({
+  getUserAuth: () => dispatch(getUserAuth())
+})
+export default connect(mapStateToProps, mapDispatchToProps)(App);
